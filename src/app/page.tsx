2c149167622e3a077af51ef771cbf425295c608a
Software Aroma <softwarearomagh@gmail.com>
2024-02-19 15:52:35 +0000

    moving

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,8 @@ import MainHeader from '@shared/components/header/MainHeader';
 import MainFooter from '@shared/components/footer/MainFooter';
 import ProductTabARea from "@shared/widgets/ProductTabARea";
 
+// preparing to clone moves
+
 
 const HomePage = () => {
     return (
